b33664820a43d755f34cc5f1225b3250720410d1
RustyRadio <danielc@my.ccsu.edu>
2015-07-27 22:13:58 -0400

    Searching works, not to parse searching

--- src/DataPull.java
+++ src/DataPull.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Map;
 
 import org.jsoup.*;
 import org.jsoup.Connection.Method;
@@ -25,7 +26,7 @@ public class DataPull {
 		System.out.println(url.toString());
 		
 		Connection.Response res = Jsoup.connect(url.toString())
-		        .data("username", "username", "password", "password")
+		        .data("username", "", "password", "")
 		       .method(Method.POST)
 		       .execute();
 		
@@ -36,6 +37,50 @@ public class DataPull {
 		else {
 			System.out.println("didnt make it");
 		}
+		res = Jsoup.connect("http://www.silverdaddies.com/search_handler.asp")
+				.cookies(res.cookies())
+				.data("zip", "10001", "radius", "20")
+				.data("profileid", "", "age_min", "", "age_max", "")
+				.data("height_feet_min", "")
+				.data("height_feet_max", "")
+				.data("height_cen_min", "")
+				.data("height_cen_max", "")
+				.data("weight_lb_min", "")
+				.data("weight_lb_max", "")
+				.data("weight_kg_min", "")
+				.data("weight_kg_max", "")
+				.data("sexpref", "one")
+				.data("top", "")
+				.data("bottom", "")
+				.data("oral_receive", "")
+				.data("oral_give", "")
+				.data("partnered", "-")
+				.data("useLocation", "")
+				.data("city", "")
+				.data("state", "NY")
+				.data("province", "")
+				.data("country", "United States")
+				.data("seek", "one")
+				.data("seek1", "")
+				.data("seek2", "")
+				.data("seek3", "")
+				.data("seek4", "")
+				.data("seek5", "")
+				.data("profile", "")
+				.data("words", "")
+				.data("text", "checked")
+				.data("image", "checked")
+				.data("online", "")
+				.data("preferage", "")
+				.data("travelers", "checked")
+				.data("travelersfuture", "")
+				.data("onlytravelers", "")
+				.data("sort", "default")
+				.method(Method.POST)
+				.execute();
+		
+		newPage = res.parse();
+		System.out.println(newPage.toString());
 		String html = "<div class='content'><center>bullshit<table cellspacing='0' cellpadding='0'><tr><td>Test 1</td><td>Test 2</td></tr></table><table cellspacing='0' cellpadding='0'><tr><td>Test 3</td><td>Test 4</td></tr></table></center></div>";
 		Elements tables = doc.select("table");
 		Element table = tables.get(1);
